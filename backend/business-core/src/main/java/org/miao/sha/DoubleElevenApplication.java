4840f6a8725419b80580ac77a44c64d27b86eb56
shiyuan2he <shiyuan4work@126.com>
2020-11-24 23:13:47 +0800

    init

---
+++ backend/business-core/src/main/java/org/miao/sha/DoubleElevenApplication.java
@@ -0,0 +1,15 @@
+
+package org.miao.sha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author heshiyuan 
+ */
+@SpringBootApplication
+public class DoubleElevenApplication {
+    public static void main(String[] args){
+        new SpringApplication(DoubleElevenApplication.class).run(args);
+    }
+}
